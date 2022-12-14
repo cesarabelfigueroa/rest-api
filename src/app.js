9958b7842f16adc3e627e3d876e817e5acbeae5a
Cesar Figueroa <cesar.figueroa@agilesolutions.com>
2022-12-14 20:05:42 -0300

    change contacts id to considerate also the customers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const bodyParser = require("body-parser");
 const { sequelize } = require("./model");
 const { getProfile } = require("./middleware/getProfile");
@@ -8,18 +9,29 @@ app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
 
 /**
- * FIX ME!
- * @returns contract by id
+ *
+ * @returns contract by profile id
  */
 app.get("/contracts/:id", getProfile, async (req, res) => {
   if (req.profile.dataValues.id) {
     const { Contract } = req.app.get("models");
     const id = req.profile.dataValues.id;
-    const contract = await Contract.findOne({ where: { ContractorId: id } });
+    const contract = await Contract.findOne({
+      where: { [Op.or]: [{ ContractorId: id }, { ClientId: id }] },
+    });
     if (!contract) return res.status(404).end();
     return res.json(contract.dataValues);
   } else {
     return res.status(404).end();
   }
 });
+
+/**
+ *
+ * @returns contract without profile id
+ */
+app.get("/contracts/", getProfile, async (req, res) => {
+  console.log(sequelize.op);
+});
+
 module.exports = app;
